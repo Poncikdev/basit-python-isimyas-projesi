4260580f6b8c43a66356d61c8f4439699bded7e7
Poncikdev <103049241+Poncikdev@users.noreply.github.com>
2022-04-06 17:42:00 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+name = input("Lütfen İsminizi Giriniz :")
+age = input("Lütfen bir Yaş Belirtiniz :")
+
+print("İsmin ve Yaşın: ", name,age)
+
+# Basit bir İsim Yaş projesi. Lisansa vs. çok gerek yok yani i  stediğini yap.
